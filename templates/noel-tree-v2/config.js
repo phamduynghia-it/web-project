f0ddb0929f178845df18989d592e607952965855
Pham Duy Nghia <nghiaitutc@gmail.com>
2026-07-12 22:56:30 +0700

    feat: config dynamic vars and add anti-devtools for noel templates and tinhcau

---
+++ templates/noel-tree-v2/config.js
@@ -0,0 +1,12 @@
+const defaultData = {
+    "template": "christmas-tree-v2",
+    "data": {
+        "title": `__TITLE__`,
+        "welcomeText": `__WELCOMETEXT__`,
+        "type_music": "link",
+        "music_id": `__MUSIC__`,
+        "backgroundMusic": `__MUSIC__`,
+        "messages": __MESSAGES__,
+        "images": __IMAGES__
+    }
+};
---
+++ templates/noel-tree-ver1/config.js
@@ -0,0 +1,12 @@
+// Cấu hình nội dung trang web
+const defaultData = {
+    "template": "christmas-tree",
+    "data": {
+        "title": `__TITLE__`,
+        "type_music": "link",
+        "music_id": `__MUSIC__`,
+        "backgroundMusic": `__MUSIC__`,
+        "messages": __MESSAGES__,
+        "images": __IMAGES__
+    }
+};
