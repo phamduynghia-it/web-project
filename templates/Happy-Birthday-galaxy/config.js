9194cd8959a691079b0c7524ef965d4fb8652a19
Pham Duy Nghia <nghiaitutc@gmail.com>
2026-07-14 23:46:58 +0700

    feat: setup happy-birthday-galaxy template for auto deploy

---
+++ templates/Happy-Birthday-galaxy/config.js
@@ -0,0 +1,25 @@
+window.__HB_PREVIEW_DATA = {
+  intro: {
+    recipientName: `__RECIPIENTNAME__`
+  },
+  music: {
+    tracks: [
+      `__MUSIC__`
+    ],
+    selectedTrackUrl: `__MUSIC__`
+  },
+  book: {
+    images: __BOOKIMAGES__
+  },
+  letter: {
+    title: `__LETTERTITLE__`,
+    body: `__LETTERBODY__`,
+    signature: `__LETTERSIGNATURE__`
+  },
+  finalGift: {
+    enabled: true,
+    wishes: __FINALWISHES__
+  }
+};
+
+window.__HB_GIFT_LOAD_PROMISE = Promise.resolve();
